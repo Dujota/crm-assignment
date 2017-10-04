c91e45ac0ac558972f91fe39e22e3042da9c6702
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-04 13:46:14 -0400

    setup the main menu mehtod and print menu method

--- crm.rb
+++ crm.rb
@@ -4,11 +4,24 @@ class CRM
 
   end
 
+#given to us in the exercise - basically always has the main menu running
   def main_menu
+    while true #repeat indefinitely - inifinite loop
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end
 
   end
 
   def print_main_menu
+    puts '[1] Add new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all contacts'
+    puts '[5] Search by attributes'
+    puts '[6] Exit'
+    puts 'Enter a number: '
 
   end
 
@@ -29,11 +42,11 @@ class CRM
   end
 
   def display_all_contacts
-    
+
   end
 
   def search_by_attribute
-    
+
   end
 
 

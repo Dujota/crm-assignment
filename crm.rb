3819ccbafdaf398c90b41b12fc0ab001301c7ff6
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-04 14:02:31 -0400

    call option mehtod defined -- this is using hte case object

--- crm.rb
+++ crm.rb
@@ -7,11 +7,10 @@ class CRM
 #given to us in the exercise - basically always has the main menu running
   def main_menu
     while true #repeat indefinitely - inifinite loop
-      print_main_menu
-      user_selected = gets.to_i
-      call_option(user_selected)
+      print_main_menu # calls print_main_menu method
+      user_selected = gets.to_i #stores user input into a local variable
+      call_option(user_selected) #passes local variable to call_option mehtod below
     end
-
   end
 
   def print_main_menu
@@ -22,10 +21,22 @@ class CRM
     puts '[5] Search by attributes'
     puts '[6] Exit'
     puts 'Enter a number: '
-
   end
+#We have two methods: print_main_menu prints out the menu and main_menu calls print_main_menu and then stores the user input (a number) into a variable called user_selected. That variable then gets passed as an argument into call_option
 
-  def call_option
+# ---------------------------------------------------------------------------
+
+# What do you think call_option should do? Remember, user_selected is going to be a number. What we want to do in this method is call other methods based on the number.
+  def call_option(user_selected)
+    case user_selected
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+    when 3 then delete_contact
+    when 4 then display_all_contacts
+    when 5 then search_by_attribute
+    when 6 then exit
+
+    end
 
   end
 
